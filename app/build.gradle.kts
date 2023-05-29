7cce336351ddb709acef5d9b68a3f5cffce04112
BoD <BoD@JRAF.org>
2023-05-29 12:51:23 +0200

    Change icon ¯\_(ツ)_/¯

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "org.jraf.android.simplewatchface3"
         minSdk = 30
         targetSdk = 33
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0.0"
     }
 
